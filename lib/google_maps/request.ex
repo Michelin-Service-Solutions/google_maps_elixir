fe5f6748d6f2c114bc787374c728fa4ba03f7c94
Batman <wolf.fen@gmail.com>
2025-10-08 16:47:15 +0000

    initial routes post work with copilot

--- lib/google_maps/request.ex
+++ lib/google_maps/request.ex
@@ -27,6 +27,38 @@ defmodule GoogleMaps.Request do
     |> format_headers()
   end
 
+
+  @spec post(String.t, keyword()) :: GoogleMaps.Response.t
+  def post(_endpoint, params) do
+    {secure, params} = Keyword.pop(params, :secure)
+    {key, params} = Keyword.pop(params, :key, api_key())
+    {headers, params} = Keyword.pop(params, :headers, [])
+    {options, params} = Keyword.pop(params, :options, [])
+
+    unless is_nil(secure) do
+      IO.puts "`secure` param is deprecated since Google requires request over SSL with API key."
+    end
+
+    # Build JSON body from remaining params
+    body = params
+      |> transform_post_params()
+      |> json_encode()
+
+    # Add API key to URL as query parameter
+    url = "https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix?key=#{key}"
+
+    # Set required headers for JSON and Google API
+    headers = [
+      {"Content-Type", "application/json"},
+      {"x-goog-api-key", key},
+      {"x-goog-fieldmask", "originIndex,distanceMeters,duration,staticDuration"}
+      | headers
+    ]
+
+    requester().post(url, headers, [body: body] ++ options)
+    |> format_headers()
+  end
+
   # Helpers
 
   defp api_key do
@@ -37,6 +69,61 @@ defmodule GoogleMaps.Request do
   defp requester do
     Application.get_env(:google_maps, :requester)
   end
+
+  defp json_encode(data) do
+    Jason.encode!(data)
+  end
+
+  # Transform params for POST request JSON body
+  defp transform_post_params(params) do
+    params
+    |> Enum.into(%{})
+    |> transform_origins()
+    |> transform_destinations()
+  end
+
+  defp transform_origins(%{origins: origins} = params) when is_list(origins) do
+    transformed_origins = Enum.map(origins, &transform_waypoint/1)
+    %{params | origins: transformed_origins}
+  end
+  defp transform_origins(params), do: params
+
+  defp transform_destinations(%{destinations: destinations} = params) when is_list(destinations) do
+    transformed_destinations = Enum.map(destinations, &transform_waypoint/1)
+    %{params | destinations: transformed_destinations}
+  end
+  defp transform_destinations(params), do: params
+
+  defp transform_waypoint({lat, lng}) when is_number(lat) and is_number(lng) do
+    %{
+      waypoint: %{
+        location: %{
+          latLng: %{
+            latitude: lat,
+            longitude: lng
+          }
+        }
+      }
+    }
+  end
+
+  defp transform_waypoint({:place_id, place_id}) do
+    %{
+      waypoint: %{
+        placeId: place_id
+      }
+    }
+  end
+
+  defp transform_waypoint(address) when is_binary(address) do
+    %{
+      waypoint: %{
+        address: address
+      }
+    }
+  end
+
+  defp transform_waypoint(waypoint) when is_map(waypoint), do: waypoint
 
   defp transform_param({type, {lat, lng}})
   when type in [:origin, :destination]
--- test/google_maps/request_test.exs
+++ test/google_maps/request_test.exs
@@ -7,6 +7,10 @@ defmodule GoogleMaps.RequestTest do
     def get(url, headers, options) do
       {:ok, %{body: url, headers: headers, options: options}}
     end
+
+    def post(url, headers, options) do
+      {:ok, %{body: url, headers: headers, options: options}}
+    end
   end
 
   setup do
@@ -59,6 +63,121 @@ defmodule GoogleMaps.RequestTest do
 
     assert capture_io(fn ->
       Request.get("foobar", params)
+    end) =~ "`secure` param is deprecated"
+  end
+
+  # POST function tests
+  test "POST constructs correct URL with API key" do
+    params = [key: "test_api_key", origins: [{34.9489252, -82.2282223}]]
+    {:ok, %{body: url}} = Request.post("distance_matrix", params)
+
+    assert %{
+      scheme: "https",
+      authority: "routes.googleapis.com",
+      path: "/distanceMatrix/v2:computeRouteMatrix",
+      query: "key=test_api_key"
+    } = URI.parse(url)
+  end
+
+  test "POST sets correct Content-Type header" do
+    params = [origins: [{34.9489252, -82.2282223}]]
+    {:ok, %{headers: headers}} = Request.post("distance_matrix", params)
+
+    assert {"Content-Type", "application/json"} in headers
+  end
+
+  test "POST sets required Google API headers" do
+    params = [key: "test_api_key", origins: [{34.9489252, -82.2282223}]]
+    {:ok, %{headers: headers}} = Request.post("distance_matrix", params)
+
+    assert {"Content-Type", "application/json"} in headers
+    assert {"x-goog-api-key", "test_api_key"} in headers
+    assert {"x-goog-fieldmask", "originIndex,destinationIndex,duration,distanceMeters,status,condition"} in headers
+  end
+
+  test "POST transforms coordinate origins correctly" do
+    params = [origins: [{34.9489252, -82.2282223}]]
+    {:ok, %{options: options}} = Request.post("distance_matrix", params)
+
+    # Extract the body from options
+    body = Keyword.get(options, :body)
+    parsed_body = Jason.decode!(body)
+
+    assert [%{
+      "waypoint" => %{
+        "location" => %{
+          "latLng" => %{
+            "latitude" => 34.9489252,
+            "longitude" => -82.2282223
+          }
+        }
+      }
+    }] = parsed_body["origins"]
+  end
+
+  test "POST transforms address destinations correctly" do
+    params = [destinations: ["1 The Parkway, Greenville, SC 29615, USA"]]
+    {:ok, %{options: options}} = Request.post("distance_matrix", params)
+
+    body = Keyword.get(options, :body)
+    parsed_body = Jason.decode!(body)
+
+    assert [%{
+      "waypoint" => %{
+        "address" => "1 The Parkway, Greenville, SC 29615, USA"
+      }
+    }] = parsed_body["destinations"]
+  end
+
+  test "POST transforms place_id destinations correctly" do
+    params = [destinations: [{:place_id, "ChIJExample123"}]]
+    {:ok, %{options: options}} = Request.post("distance_matrix", params)
+
+    body = Keyword.get(options, :body)
+    parsed_body = Jason.decode!(body)
+
+    assert [%{
+      "waypoint" => %{
+        "placeId" => "ChIJExample123"
+      }
+    }] = parsed_body["destinations"]
+  end
+
+  test "POST includes other parameters in JSON body" do
+    params = [
+      origins: [{34.9489252, -82.2282223}],
+      destinations: ["1 The Parkway, Greenville, SC 29615, USA"],
+      travelMode: "DRIVE",
+      routingPreference: "TRAFFIC_AWARE"
+    ]
+    {:ok, %{options: options}} = Request.post("distance_matrix", params)
+
+    body = Keyword.get(options, :body)
+    parsed_body = Jason.decode!(body)
+
+    assert parsed_body["travelMode"] == "DRIVE"
+    assert parsed_body["routingPreference"] == "TRAFFIC_AWARE"
+  end
+
+  test "POST supports custom headers" do
+    params = [
+      key: "custom_key",
+      origins: [{34.9489252, -82.2282223}],
+      headers: [{"Authorization", "Bearer token123"}]
+    ]
+    {:ok, %{headers: headers}} = Request.post("distance_matrix", params)
+
+    assert {"Content-Type", "application/json"} in headers
+    assert {"x-goog-api-key", "custom_key"} in headers
+    assert {"x-goog-fieldmask", "originIndex,destinationIndex,duration,distanceMeters,status,condition"} in headers
+    assert {"Authorization", "Bearer token123"} in headers
+  end
+
+  test "POST deprecates `secure` param" do
+    params = [secure: false, origins: [{34.9489252, -82.2282223}]]
+
+    assert capture_io(fn ->
+      Request.post("distance_matrix", params)
     end) =~ "`secure` param is deprecated"
   end
 end
